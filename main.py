b43e802fd4928bda54e2b0330808d16144ab51cd
Joshua Thomas <134388878+Joshuathomas22135@users.noreply.github.com>
2026-05-13 19:55:52 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# Store two numbers of your choice in two different variables. 
+# Now perform all the Arithmetic operations on those two numbers and print the results.
+
+num1 = 100
+num2 = 400
+
+sum = num1 + num2
+sub = num2 - num1
+multi = num1 * num2
+div = num1 / num2
+flor = num1 / num2
+exp = num1 ** num2
+
+print(sum)
+print(sub)
+print(multi)
+print(div)
+print(flor)
+print(exp)
